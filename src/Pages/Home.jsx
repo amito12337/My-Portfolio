8e170a9adf6a2b9b73f8c490cce4755b8b31d69c
Abdessamad Amaitit <aamaitit@gmail.com>
2024-01-01 11:59:06 +0000

    add island
    add assets folder and add island to the home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
-
+import React from "react";
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import Loader from "../components/Loader";
+import Island from "../models/Island";
+{
+  /* <div className="absolute-top-28 left-0 right-0 z-10 flex items-center justify-center">
+  PouPUP
+</div> */
+}
 function Home() {
+  const adjustIslandForScreenSize = () =>{
+    let screenScale = null
+    let screenPosition =  [0, -6.5, -43];
+    let rotation = [0.1,4.8,0]
+    if(window.innerWidth < 768) {
+      screenScale = [0.5,0.9,0.9];
+    } else {
+      screenScale = [1, 11, 1];
+    }
+    return [screenScale,screenPosition,rotation];
+  }
+  const [islandScale,islandPosition,islandRotation] = adjustIslandForScreenSize()
   return (
-    <div>Home</div>
-  )
+    <section className="w-full h-screen relative">
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}>
+        <Suspense fallback={<Loader />}>
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
+          <hemisphereLight skycolor="#~b1e1ff" groundColor={"#000"} intensity={1}/>
+          <Island
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation}
+          />
+        </Suspense>
+      </Canvas>
+    </section>
+  );
 }
 
-export default Home
+export default Home;
