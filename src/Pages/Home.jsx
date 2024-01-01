60323a102bb54c61d08a02db4d430cb8ebbc841c
Abdessamad Amaitit <aamaitit@gmail.com>
2024-01-01 13:15:14 +0000

    edit islandScale

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -16,7 +16,7 @@ function Home() {
     if(window.innerWidth < 768) {
       screenScale = [0.5,0.9,0.9];
     } else {
-      screenScale = [1, 11, 1];
+      screenScale = [1, 1, 1];
     }
     return [screenScale,screenPosition,rotation];
   }
@@ -25,7 +25,7 @@ function Home() {
     <section className="w-full h-screen relative">
       <Canvas
         className="w-full h-screen bg-transparent"
-        camera={{ near: 0.1, far: 1000 }}>
+        camera={{ near: 0.1, far: 900 }}>
         <Suspense fallback={<Loader />}>
           <directionalLight position={[1, 1, 1]} intensity={2} />
           <ambientLight intensity={0.5} />
