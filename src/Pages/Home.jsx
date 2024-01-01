04247687163d19fde2e3137f507ede7418ba3da7
Abdessamad Amaitit <aamaitit@gmail.com>
2024-01-01 16:52:49 +0000

    raplace the plane

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -23,17 +23,20 @@ function Home() {
     }
     return [screenScale,screenPosition,rotation];
   }
-  const adjustPlaneForScreenSize = () => {
-    let screenScale ,screenPosition ;
-    if (window.innerWidth < 768) {
-      screenPosition = [1.5, 1.5, 1.5];
-      screenScale = [0, 1.5, 0];
-    } else {
-      screenScale = [1,1,1.2];
-      screenPosition = [0, -1,1.4];
-    }
-    return [screenScale, screenPosition];
-  };
+    const adjustPlaneForScreenSize = () => {
+      let screenScale, screenPosition;
+
+      // If screen width is less than 768px, adjust the scale and position
+      if (window.innerWidth < 768) {
+        screenScale = [1.5, 1.5, 1.5];
+        screenPosition = [0, -1.5, 0];
+      } else {
+        screenScale = [3, 3, 3];
+        screenPosition = [0, -4, -4];
+      }
+
+      return [screenScale, screenPosition];
+    };
   const [islandScale,islandPosition,islandRotation] = adjustIslandForScreenSize()
   const [planeScale,planePosition] = adjustPlaneForScreenSize()
   return (
