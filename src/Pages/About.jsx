7e34a06a0d633268bf9095ef2ab5a6dc294cb196
Abdessamad Amaitit <aamaitit@gmail.com>
2024-01-03 17:38:52 +0000

    Add My Experience With Problems

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-import { skills } from './../constants';
+import { skills, experiences } from "./../constants";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
 
 function About() {
   return (
@@ -19,15 +23,72 @@ function About() {
       <div className="py-10 flex flex-col">
         <h3 className="subhead-text">My Skills</h3>
         <div className="mt-16 flex flex-wrap gap-12">
-          {skills.map((skill) => (
-            <div className="block-container w-20 h-28 flex justify-center items-center">
-              <div className="btn-back rounded-xl"/>
+          {skills.map((skill, index) => (
+            <div
+              key={index}
+              className="block-container w-20 h-28 flex justify-center items-center">
+              <div className="btn-back rounded-xl" />
               <div className="btn-front rounded-xl flex justify-center items-center">
-                <img src={skill.imageUrl} alt={skill.name} className="w-1/2 h-1/2 object-contain"/>
+                <img
+                  src={skill.imageUrl}
+                  alt={skill.name}
+                  className="w-1/2 h-1/2 object-contain"
+                />
               </div>
             </div>
           ))}
         </div>
+      </div>
+      <div className="py-16">
+        <h3 className="subhead-text">Work Experience</h3>
+        <p className="mt-5 flex flex-col gap-3 text-slate-500">
+          I worked with all sorts of companies, leveling up my skills and
+          learning up my with smart people. Here's the rundown :
+        </p>
+      </div>
+      <div className="mt-12 flex">
+        <VerticalTimeline>
+          {experiences.map((experience) => {
+            <VerticalTimelineElement
+              date={experience.date}
+              icon={
+                <div>
+                  <img
+                    src={experience.icon}
+                    alt={experience.company_name}
+                    className="w-1/2 h-[60% object-contain"
+                  />
+                </div>
+              }
+              iconStyle={{
+                background:experience.iconBg
+              }}
+              contentStyle={{
+                borderBottom:"8px",
+                borderStyle:"solid",
+                borderBottomColor:experience.iconBg
+              }}
+              key={experience.company_name}>
+              <div>
+                <h3 className="text-black text-xl font-poppins font-semibold">
+                  {experience.title}
+                </h3>
+                <p className="text-black-500 font-medium font-base m-0">
+                  {experience.company_name}
+                </p>
+              </div>
+              <ul className="my-5 list-disc ml-5 space-y-2 ">
+                {experience.points.map((point, index) => {
+                  <li
+                    key={index}
+                    className="text-black-500/50 font-normal pl-1 text-sm">
+                    {point}
+                  </li>;
+                })}
+              </ul>
+            </VerticalTimelineElement>;
+          })}
+        </VerticalTimeline>
       </div>
     </section>
   );
