16b7974e988a810883150eacb68604a720b2b527
Abdessamad Amaitit <aamaitit@gmail.com>
2024-01-05 15:00:20 +0000

    edit loader

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,12 +3,10 @@ import React from 'react'
 
 function Loader() {
   return (
-   <Html>
-    <div className="flex justify-center items-center">
-        <div className="w-20 h-20 border-2 border-opacity-20 border-blue-500 rounded-full animate-spin" />
-   </div>
-   </Html>
-  )
+    <Html>
+      <div class="loader"></div>
+    </Html>
+  );
 }
 
 export default Loader
